d26ab1377689086e568bf0b167da938fa713a265
Lucas Camargo <nascimentocamargolucas@gmail.com>
2025-11-03 19:50:08 -0300

    Criado caso de teste para envio de e-mail.

---
+++ tests/e2e/sendEmail.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
+
+const firstName = faker.person.firstName();
+const phone = faker.phone.number();
+const email = faker.internet.email();
+
+test('test', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('link', { name: 'contact', exact: true }).click();
+  await page.locator('#name').fill(firstName);
+  await page.locator('#email').fill(email);
+  await page.locator('#phone').fill(phone);
+  await page.locator('#message').fill('Enviando email teste.');
+  await page.getByRole('button', { name: 'Send to Customer Care' }).click();
+  await expect(page.getByText('A Customer Care')).toBeVisible();
+});
