ba74f49e310d0e27aec3213402e6cd012db90846
Lucas Camargo <nascimentocamargolucas@gmail.com>
2025-11-11 13:40:19 -0300

    Configurando PageObjects

--- page/LoginPage.ts
+++ page/LoginPage.ts
@@ -1,8 +1,24 @@
-import { faker } from '@faker-js/faker';
+import { expect, Page } from '@playwright/test';
+import users from '../tests/data/users.json';
 
 export class LoginPage {
-    
-    // clickRegister() {
-    //     cy.contains('Register').click();
-    // }
+  /**
+   * @param {Page} page
+   */
+  constructor(page) {
+    this.page = page;
+  }
+
+  async fillUsername() {
+    await page.locator('input[name="username"]').fill(users.validUser.username);
+  }
+
+   async fillPassword() {
+    await page.locator('input[type="password"]').fill(users.validUser.password);
+  }
+
+   async ClickEnter() {
+    await page.getByRole('button', { name: 'Log In' }).click();
+        await expect(page.getByText('Balance includes deposits that may be subject to holds')).toBeVisible();
+  }
 }
--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -1,10 +1,12 @@
 import { test, expect } from '@playwright/test';
 import users from '../data/users.json';
+import { LoginPage}  from '../../page/LoginPage';
 
 test('Login Sucessful', async ({ page }) => {
-  awaitLpage.goto('/');
-  await page.locator('input[name="username"]').fill(users.validUser.username);
-  await page.locator('input[type="password"]').fill(users.validUser.password);
-  await page.getByRole('button', { name: 'Log In' }).click();
-  await expect(page.getByText('Balance includes deposits that may be subject to holds')).toBeVisible();
+
+  await page.goto('/');
+  LoginPage.
+  LoginPage.
+  LoginPage.
+  
 });
